014630c55c7f752bdaa8f1ec50d59d90ad6e7300
Alden Transfiguracion <alden51700@gmail.com>
2025-08-14 12:05:05 -0600

    Add task card component with editing functionality and styles; integrate into tasks page for improved task management.

---
+++ src/app/components/task-card/task-card.component.ts
@@ -0,0 +1,67 @@
+import { Component, inject, input, linkedSignal, signal, ViewChild } from '@angular/core';
+import type { Task } from '../../../../prisma/client';
+import { TRPC_CLIENT } from '../../utils/trpc.client';
+import { trpcResource } from '@fhss-web-team/frontend-utils';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatIcon } from '@angular/material/icon';
+import { MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
+
+@Component({
+  selector: 'task-card',
+  standalone: true,
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatCardModule, MatButtonModule, FormsModule, MatIcon, MatMenuModule],
+  templateUrl: './task-card.component.html',
+  styleUrl: './task-card.component.scss'
+})
+export class TaskCardComponent {
+  editMode = signal(false)
+  initialTaskValue = input.required<Task>();
+
+  @ViewChild(MatMenuTrigger) trigger!: MatMenuTrigger;
+  OpenMenu() {
+    this.trigger.openMenu();
+  } 
+
+  
+  newTitle = linkedSignal(() => this.initialTaskValue().title);
+  newDescription = linkedSignal(() => this.initialTaskValue().description);
+  newStatus = linkedSignal(() => this.initialTaskValue().status);
+  
+  trpc = inject(TRPC_CLIENT);
+  taskCardState = trpcResource(
+    this.trpc.tasks.updateTask.mutate,
+    () => ({
+      taskId: this.initialTaskValue().id,
+      newTitle: this.newTitle(),
+      newDescription: this.newDescription(),
+      newStatus: this.newStatus(),
+    }),
+    { valueComputation: () => this.initialTaskValue() }
+  );
+
+  saveChanges() {
+    this.taskCardState.value.update((prev) => {
+      if (!prev) return prev;
+      return {
+        ...prev,
+        title: this.newTitle(),
+        description: this.newDescription(),
+        status: this.newStatus(),
+      }
+    })
+    this.taskCardState.refresh();
+    this.editMode.set(false);
+  }
+  
+  cancelChanges() {
+    this.newTitle.set(this.initialTaskValue().title);
+    this.newDescription.set(this.initialTaskValue().description);
+    this.newStatus.set(this.initialTaskValue().status);
+    this.editMode.set(false);
+  }
+}
--- src/app/pages/tasks/tasks.page.ts
+++ src/app/pages/tasks/tasks.page.ts
@@ -3,10 +3,11 @@ import { TRPC_CLIENT } from '../../utils/trpc.client';
 import { trpcResource } from '@fhss-web-team/frontend-utils';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { TaskCardComponent } from '../../components/task-card/task-card.component';
 
 @Component({
   selector: 'app-tasks',
-  imports: [MatProgressSpinnerModule, MatPaginator],
+  imports: [MatProgressSpinnerModule, MatPaginator, TaskCardComponent],
   templateUrl: './tasks.page.html',
   styleUrl: './tasks.page.scss'
 })
@@ -16,7 +17,6 @@ export class TasksPage {
   }
 
   trpc = inject(TRPC_CLIENT);
-
   PAGE_SIZE = 12;
   pageOffset = signal(0);
 
