cd24ea1651d3cbf748938b8ba2d1d41c7ebcd689
Alden Transfiguracion <alden51700@gmail.com>
2025-08-14 12:46:28 -0600

    Add create task functionality to tasks page; implement form for task creation and integrate with task management logic.

--- src/app/pages/tasks/tasks.page.ts
+++ src/app/pages/tasks/tasks.page.ts
@@ -4,10 +4,15 @@ import { trpcResource } from '@fhss-web-team/frontend-utils';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { TaskCardComponent } from '../../components/task-card/task-card.component';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-tasks',
-  imports: [MatProgressSpinnerModule, MatPaginator, TaskCardComponent],
+  imports: [MatProgressSpinnerModule, MatPaginator, TaskCardComponent, MatCardModule, FormsModule, MatFormFieldModule, MatInputModule,],
   templateUrl: './tasks.page.html',
   styleUrl: './tasks.page.scss'
 })
@@ -34,5 +39,18 @@ export class TasksPage {
       taskId: taskId,
     });
     this.taskResource.refresh();
+  }
+
+  createdTitle = signal('New Task');
+  createdDescription = signal('New Task Description');
+
+  isCreateTask = signal(false);
+  createTask() {
+    this.trpc.tasks.createTask.mutate({
+      title: this.createdTitle(),
+      description: this.createdDescription(),
+    });
+    this.taskResource.refresh();
+    this.isCreateTask.set(false);
   }
 }
